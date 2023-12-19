e562790e856963c4885e52fc5d6b9d515361a8d8
hasarangad <hasarangadias@gmail.com>
2023-12-19 09:13:42 +0530

    JobApplication.php
    Navbar and footer

--- jobApplication.php
+++ jobApplication.php
@@ -5,8 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Application</title>
+    <link rel="stylesheet" type="text/css" href="./CSS/styleJobApplication.css">
 </head>
 <body>
-    
+<div class="header">
+        <div class="logo">
+            <img src="./Images/Logo.png" alt="Logo">
+            <a href="">CareerBridge</a>
+        </div>
+
+        <div class="navbar">
+            <a href="settingE.php">Settings</a>
+            <a href="#">Notification</a>
+            <a href="#">Company</a>
+            <a href="jobPage.php">Find a Job</a>
+            <a href="homeE.php">Home</a>
+        </div>    
+    </div>
+
+    <footer class="ftr">
+        <p>
+            &copy; 2023 CareerBridge. All rights reserved.
+        </p>
+    </footer>
 </body>
 </html>
