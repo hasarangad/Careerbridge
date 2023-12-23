bc4e921f90379230ff15f806b6e80f9c877a9a5e
hasarangad <hasarangadias@gmail.com>
2023-12-23 10:49:41 +0530

    Styles
    Forgot password

--- forgotPW.php
+++ forgotPW.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +9,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CareerBridge | Forgot Password</title>
+    <link rel="stylesheet" type="text/css" href="CSS/forgotpwStyle.css">
 </head>
 <body>
-    
+    <div id="div1" class="firstDiv">
+        <form action="" method="post">
+            <h2>Forgot Password</h2>
+            <div class="inputBox">
+                <div class="inputField">
+                    <input type="text" name="fName" id="" required>
+                    <span>First Name</span>
+                    <i></i>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div id="div2" class="secondDiv">
+        <!-- Content of the second div -->
+        <p>This is the second div.</p>
+    </div>
+
+    <script>
+        function showSecondDiv() {
+            // Hide the first div
+            document.getElementById('div1').style.display = 'none';
+            // Show the second div
+            document.getElementById('div2').style.display = 'block';
+        }
+    </script>
+  </script>
 </body>
 </html>
