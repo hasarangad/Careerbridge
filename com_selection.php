83ee61f6e56e088eb2ae6d29caae4e83b24af31b
hasarangad <hasarangadias@gmail.com>
2023-12-25 12:49:39 +0530

    Create com_selection.php
    new php file for add job button

---
+++ com_selection.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company  Selection</title>
+    <link rel="stylesheet" type="text/css" href="CSS/selection.css">
+</head>
+<body>
+    <div class="rapper">
+    
+        <div class="img">
+            <img src="./Img/Logo.png" alt="Logo">
+        </div>
+    
+        <div class="title">
+            <h1>
+                Do yo have a company
+            </h1>
+        </div>
+    
+        <div class="btn">
+
+            <div class="yes">
+                <a href= "add_jobpost.php"> 
+                    <button class="button">Yes</button>
+                </a>
+            </div>
+
+            <div class="no">
+                <a href="add_company.php">
+                    <button class="button">No</button>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
