91490b522c2c841f4b023b5169e8a21792c26f6c
hasarangad <hasarangadias@gmail.com>
2023-12-27 11:02:28 +0530

    Create navBarS.php
    New

---
+++ navBarS.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styleNavF.css">
+</head>
+<body>
+    <nav class="header">
+        <div class="logo">
+            <img src="./Images/Logo.png" alt="Logo">
+            <a href="#">CareerBridge</a>
+        </div>
+        <div class="navbar">
+            <a href="settingS.php">Settings</a>
+            <a href="notificationS.php">Notification</a>
+            <a href="#">Company</a>
+            <a href="jobPageS.php">Find a Job</a>
+            <a href="homeES.php">Home</a>
+        </div>
+        
+    </nav>
+
+</body>
+</html>
