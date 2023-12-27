15ac97bbe2a86a92aa386524d6e5e19ef60be76f
hasarangad <hasarangadias@gmail.com>
2023-12-27 10:01:58 +0530

    Update viewApplication.php
    view php set varibales

--- viewApplication.php
+++ viewApplication.php
@@ -3,7 +3,11 @@
 
     include 'dbh.inc.php';
     
+    //variables
     $uName;
+    $applicationId;
+
+    //check user loged in or not
     if(!isset($_SESSION["uName"])){
         header('Location: login.php');  
     }
@@ -11,4 +15,11 @@
         $uName = $_SESSION['uName'];
     }
     
+    //check id is set or not
+    if(isset($_GET['id'])){
+        $applicationId = mysqli_real_escape_string($conn, $_GET['id']);
+    }
+    else{
+        header("Location: applied_jobs_view.php");
+    }
 ?>
