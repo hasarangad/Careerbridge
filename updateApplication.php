1304886fec1915f75a437ddd738acae82dbd8284
hasarangad <hasarangadias@gmail.com>
2023-12-28 11:30:40 +0530

    update
    Done

--- updateApplication.php
+++ updateApplication.php
@@ -52,35 +52,66 @@
         <form action="" method="post" enctype="multipart/form-data">
             <div class="inlinecontentC">
                 <h3>Full Name : </h3>
-                <input type="text" name="lName" id="" value="<?php echo $row['full_name']?>">
+                <input type="text" name="fullName" id="" value="<?php echo $row['full_name']?>">
             </div>
 
             <div class="inlinecontentC">
                 <h3>Contact Number : </h3>
-                <input type="text" name="lName" id="" value="<?php echo $row['contact_number']?>">
+                <input type="text" name="contact" id="" value="<?php echo $row['contact_number']?>">
             </div>
 
             <div class="inlinecontentC">
                 <h3>Email Address : </h3>
-                <input type="text" name="lName" id="" value="<?php echo $row['email']?>">
+                <input type="text" name="email" id="" value="<?php echo $row['email']?>">
             </div>
 
             <div class="inlinecontentC">
                 <h3>Date of birth : </h3>
-                <input type="text" name="lName" id="" value="<?php echo $row['dob']?>">
+                <input type="text" name="dob" id="" value="<?php echo $row['dob']?>">
             </div>
 
             <div class="inlinecontentC">
                 <h3>Change CV : </h3>
                 <input type="File" name="file" value="">                
             </div>
-
+            
+            <input type="submit" value="Update" name="update">
         </form>
 
         <?php
             }
         }?>
     </div>
+    <?php
+        if(isset($_POST['update'])){
+            $fullName = mysqli_real_escape_string($conn, $_POST['fullName']);
+            $contact = mysqli_real_escape_string($conn, $_POST['contact']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $dob = mysqli_real_escape_string($conn, $_POST['dob']);
+
+            #file name with a random number so that similar dont get replaced
+            $pname = $uName."-".$_FILES["file"]["name"];
+        
+            #temporary file name to store file
+            $tname = $_FILES["file"]["tmp_name"];
+            
+            #upload directory path
+            $uploads_dir = 'uploads/';
+
+            $file_path = $uploads_dir . $pname;
+            #TO move the uploaded file to specific location
+            move_uploaded_file($tname, $uploads_dir.'/'.$pname);
+
+            $sql = "UPDATE job_applications SET full_name='$fullName',contact_number='$contact',email='$email',dob='$dob',resume_path='$file_path' WHERE application_id='$applicationId'";
+ 
+            if(mysqli_query($conn,$sql)){
+        
+            }
+            else{
+                echo "Error";
+            }
+        }   
+    ?>
     <?php include 'footer.php';?>
 </body>
 </html>
