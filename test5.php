ed472d87c6d8102f9e0998e3e8a36713e9b0614a
hasarangad <hasarangadias@gmail.com>
2023-12-31 08:39:53 +0530

    New File
    for checking cv

--- test5.php
+++ test5.php
@@ -1,53 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>File Upload</title>
-</head>
-<body>
- 
-<form method="post" enctype="multipart/form-data">
-    <label>Title</label>
-    <input type="text" name="title">
-    <label>File Upload</label>
-    <input type="File" name="file">
-    <input type="submit" name="submit">
- 
- 
-</form>
- 
-</body>
-</html>
- 
-<?php 
-include 'dbh.inc.php';
- 
-if (isset($_POST["submit"]))
- {
-     #retrieve file title
-    $title = $_POST["title"];
-     
-    #file name with a random number so that similar dont get replaced
-     $pname = rand(1000,10000)."-".$_FILES["file"]["name"];
- 
-    #temporary file name to store file
-    $tname = $_FILES["file"]["tmp_name"];
-   
-     #upload directory path
-    $uploads_dir = 'Images';
-    #TO move the uploaded file to specific location
-    move_uploaded_file($tname, $uploads_dir.'/'.$pname);
- 
-    #sql query to insert into database
-    $sql = "INSERT into fileup(title,image) VALUES('$title','$pname')";
- 
-    if(mysqli_query($conn,$sql)){
- 
-    echo "File Sucessfully uploaded";
-    }
-    else{
-        echo "Error";
-    }
+<?php
+$fileUrl = 'https://example.com/path/to/file.zip';
+$destination = 'downloaded_file.zip';
+
+// Download the file
+$fileContents = file_get_contents($fileUrl);
+
+// Save the file to the specified destination
+file_put_contents($destination, $fileContents);
+
+// Check if the file was downloaded successfully
+if (file_exists($destination)) {
+    echo 'File downloaded successfully!';
+} else {
+    echo 'Failed to download the file.';
 }
- 
- 
 ?>
