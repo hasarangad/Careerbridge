29d625fcb3897e6a10c9da45167a57fe3278c6c7
hasarangad <hasarangadias@gmail.com>
2023-12-31 11:55:10 +0530

    checking cv
    Done

--- checkCV.php
+++ checkCV.php
@@ -44,17 +44,17 @@
                     mysqli_stmt_bind_param($stmt1,"s", $jobID);
                     mysqli_stmt_execute($stmt1);
                     $result1 = mysqli_stmt_get_result($stmt1);
-                    while($row = mysqli_fetch_assoc($result1)){
+                    while($row1 = mysqli_fetch_assoc($result1)){
 
         ?>        
         
         <div class="box">
-            <h3>CV / Resume for job id <?php echo $row['job_id'];?> : </h3>
-            <!-- <div class="button">
-                <a href="viewApplication.php?id=<?php echo $applicationId;?>"><input type="submit" value="Read CV" name="rCv"></a>
-                <a href="updateApplication.php?id=<?php echo $applicationId;?>"><input type="submit" value="Update CV" name="uCv"></a>
-                <a href="deleteApplication.php?id=<?php echo $applicationId;?>"><input type="submit" value="Delete CV" name="dCv"></a>
-            </div> -->
+            <h3>New CV / Resume for job id <?php echo $row1['job_id'];?> by <?php echo $row1['full_name'];?> : </h3>
+            <div class="button">
+                <a href="viewCV.php?id=<?php echo $jobID;?>"><input type="submit" value="Full Details" name="fullCV"></a>
+                <!-- <a href="updateApplication.php?id=<?php echo $applicationId;?>"><input type="submit" value="Update CV" name="uCv"></a>
+                <a href="deleteApplication.php?id=<?php echo $applicationId;?>"><input type="submit" value="Delete CV" name="dCv"></a> -->
+            </div>
         </div>
         <?php
                     }
