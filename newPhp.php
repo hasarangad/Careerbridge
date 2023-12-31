e432c78d7c676efc0acd969a230929d25c807bf6
hasarangad <hasarangadias@gmail.com>
2023-12-31 08:04:24 +0530

    Create newPhp.php
    new file

---
+++ newPhp.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: black;
+            color: white;
+            margin: auto;
+            align-items: center;
+
+        }
+        input{
+            width:20%;
+
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="fName">Full Name : </label>
+        <input type="text" name="" id=""><br>
+
+        <label for="">DOB :</label>
+        <input type="date" name="" id=""><br>
+
+        <label for="">Email :</label>
+        <input type="text" name="" id=""><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
