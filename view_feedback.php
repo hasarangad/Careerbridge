2641f63a1c1f66696220b75036b8dfb79b5ea1bb
hasarangad <hasarangadias@gmail.com>
2024-01-03 12:20:12 +0530

    Update view_feedback.php

--- view_feedback.php
+++ view_feedback.php
@@ -11,8 +11,7 @@
     <meta name="viewport" content="width=fo, initial-scale=1.0">
     <title>update_feedback</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/update_feedback.css">
-    <link rel="stylesheet" href="styleNavF.css">
+    <link rel="stylesheet" href="CSS/update_feedback.css">
 </head>
 
 <body>
