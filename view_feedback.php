2eab7169e40af9555da3ee7469d6c6d96ce0baac
hasarangad <hasarangadias@gmail.com>
2024-01-03 12:04:35 +0530

    Create view_feedback.php

---
+++ view_feedback.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=fo, initial-scale=1.0">
+    <title>update_feedback</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/update_feedback.css">
+    <link rel="stylesheet" href="styleNavF.css">
+</head>
+
+<body>
+    <!--Navigation bar-->
+    <?php
+        include ('navBar.php');
+    ?>
+
+<div class="container">
+    <div class="title">
+        <h3>You have the option to edit your feedback</h3>
+    </div>
+    <div class="rapper">
+
+        <?php
+            include('php/connection.php');
+            include ('php/update_elem.php');
+            session_start();
+    
+            $id = $_SESSION['user_id'];
+
+            $sql ="SELECT * FROM reviews WHERE id={$id}";
+
+            $result = mysqli_query($connection,$sql);
+
+            if(mysqli_num_rows($result)>0){
+                while($rows =mysqli_fetch_assoc($result)){
+                    up_elem($rows['company_name'],$rows['comment_id'],$rows['user_name'],$rows['email'],$rows['comment'],$rows['com_id']);     
+                }   
+            }   
+        ?>
+
+    </div>
+
+
+<!-- Footer --> 
+<?php
+    include('footer.php');
+
+?>
+
+</div>
+
+</body>
+</html>
+
+<?php
+    mysqli_close($connection);
+?>
+
