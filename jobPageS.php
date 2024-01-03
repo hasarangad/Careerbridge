17ecd24a18144f5ad12262e7564edf4ce2219cfe
hasarangad <hasarangadias@gmail.com>
2024-01-03 11:28:17 +0530

    Job post S

--- jobPageS.php
+++ jobPageS.php
@@ -3,7 +3,7 @@
     session_start();
     include 'dbh.inc.php';
 
-    include './php/job_post_element.php';
+    include './php/job_post_elementS.php';
     
 
         if(!isset($_SESSION["uName"])){
@@ -18,69 +18,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/find_job.css">
 
-     <!--font Awsome-->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+    <!--font Awsome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>Find job</title>
 
 </head>
 <body>
-        <?php include 'navBarS.php';?>
-        <!-- Navigation -->
-    
+   <!-- Navigation -->
+   <?php
+       include('navBarS.php');
+   ?>
+
 <div class="rapper">
 
     <div class="section">
-            <div class="main-caption">
-                <p>Find great Job.....</p>
-            </div>
-            
-            <div class="sub-caption">
-                <p>Get access to millions of company  vecancies</p>
-            </div>
+
+        <div class="main-caption">
+            <h1>Find great places to work</h1>
+        </div>
+        
+        <div class="sub-caption">
+            <h3>Get access to millions of company Jobs</h3>
+        </div>
+
     </div>
 
     <div class="search">
-        <form action="jobPageS.php" method="get">
-            <input type="text" name="search" id="" placeholder="Enter the job Title" autofocus>
-            <button type ="submit" name ="submit">Find the job</button>
+
+        <form action="" method="get">
+
+            <div class="form">
+                <div class="input">
+                    <input type="text" name="search" placeholder="Enter campany name" autofocus>
+                </div>
+
+                <div class="btn">
+                    <button type ="submit" name ="submit">Find Companies</button>
+                </div>
+
+            </div>
+
         </form>
+
     </div>
+
+    <div class="line"></div>
 
 
     <div class="main">
         <?php
             //company search
             if(isset($_GET['search'])){
-                $search = mysqli_real_escape_string($conn,$_GET['search']);
+                $search = mysqli_real_escape_string($conn, $_GET['search']);
                 $sql ="SELECT * FROM job WHERE (job_title LIKE '%{$search}%') ORDER BY job_title ";
             }
             else{
                 $sql ="SELECT * FROM job";
             }
     
-            $result = mysqli_query($conn, $sql);
+            $result = mysqli_query($conn,$sql);
 
             if(mysqli_num_rows($result)>0){
                 while($rows =mysqli_fetch_assoc($result)){
-                    elements($rows['job_id'],$rows['company_name'],$rows['job_title'],$rows['job_location'],$rows['job_category'],$rows['monthly_Salary'],$rows['sort_description']);        
+                    $userName = $rows['userName'];
+                    $query = "SELECT * FROM user WHERE userName ='$userName'";
+                    $res = mysqli_query($conn,$query);
+                    
+                    $first_name = '';
+                    $last_name = '';
+
+                    if(mysqli_num_rows($result)>0){
+                        while($row = mysqli_fetch_assoc($res)){
+                            $first_name = $row['fname'];
+                            $last_name = $row['lname'];
+                        }
+                    }
+
+                    elements($rows['userName'],$rows['job_id'],$rows['company_name'],$rows['job_title'],$rows['job_location'],$rows['job_category'],$rows['monthly_Salary'],$rows['sort_description'],$first_name,$last_name);        
                 }   
             }
         ?>
 
     </div>
     
-    <!-- <div class="rate">
-        <a href= "com_selection.php">
-            <button type="submit" name="submit">
-                <i class="fa-solid fa-plus"></i>
-                <p>Add job post<p>
-            </button>
-        </a>
-    </div> -->
-
 </div>
-<?php include 'footer.php';?>
+    
+
+
+
+<?php
+    include('footer.php');
+
+?>
+
+
     
 </body>
 </html>
---
+++ php/job_post_elementS.php
@@ -0,0 +1,61 @@
+<?php
+
+function elements($id,$row,$company_name,$job_title,$job_location,$job_category,$monthly_Salary,$sort_description,$firstname,$lastname){
+   
+   
+    $elemnt = 
+
+    "
+<div class=\"container\">
+
+    
+    <div class=\"row1\">
+        <h3>$job_title</h3>
+    </div>
+
+        
+    <div class=\"row2\">
+        <div class=\"block2\">
+            <strong>Job category</strong>
+            <p>$job_category</p>
+        </div>
+        <div class=\"block1\">
+            <strong>Location</strong>
+            <p>$job_location</p>
+        </div>
+    </div>
+
+    <div class = \"row3\">
+
+        <div class=\"block3\">
+            <strong>MonthlySalary</strong>
+            <p>$monthly_Salary</p>
+        </div>
+
+        <div class=\"block4\">
+            <strong>Company name</strong>
+            <p>$company_name</p>
+        </div>
+    </div>
+    
+    <div class=\"row4\">
+        <strong>Job description :</strong>
+        <p>$sort_description</p>
+    </div>
+
+    <div class=\"posted\">
+        posted by <a href=\"profileE.php?userName={$id}\">@$firstname $lastname</a>
+    </div>
+
+    <div class=\"btn\">
+        <a href=\"full_job_post.php?id={$row}\"><button>Full Details</button></a>
+    </div>
+     
+
+</div>"
+    
+;
+echo $elemnt;}
+
+?>
+
