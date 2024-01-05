b34aac403671a6c8752c46bb79dfe8cae590f998
hasarangad <hasarangadias@gmail.com>
2024-01-05 20:01:44 +0530

    Update updateInterviewE.php

---
+++ updateInterviewE.php
@@ -0,0 +1,104 @@
+<?php
+    session_start();
+
+    include 'dbh.inc.php';
+    
+    //variables
+    $uName;
+    $applicationId;
+
+    //check user loged in or not
+    if(!isset($_SESSION["uName"])){
+        header('Location: login.php');  
+    }
+    else{
+        $uName = $_SESSION['uName'];
+    }
+    
+    //check application id is set or not
+    if(isset($_GET['id'])){
+        $applicationId = mysqli_real_escape_string($conn, $_GET['id']);
+    }
+    else{
+        header("Location: applied_jobs_view.php");
+    }
+
+    $sql = "SELECT * FROM interviews WHERE application_id = ?";
+    $stmt = mysqli_stmt_init($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Interview</title>
+    <link rel="stylesheet" type="text/css" href="CSS/myApplicationStyle.css">
+</head>
+<body>
+    <?php include 'navBarS.php';?>
+
+    <div class="container">
+        <?php
+        if(mysqli_stmt_prepare($stmt, $sql)){
+            mysqli_stmt_bind_param($stmt,"s", $applicationId);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            while($row = mysqli_fetch_assoc($result)){
+                $applicationId = $row['application_id'];
+        ?>        
+        
+        <h1>Interview For Application ID <?php echo $row['application_id'];?>  </h1>
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="inlinecontentC">
+                <h3>Email Address : </h3>
+                <input type="text" name="email" id="" value="<?php echo $row['email']?>">
+            </div>
+
+            <div class="inlinecontentC">
+                <h3>Date : </h3>
+                <input type="text" name="dob" id="" value="<?php echo $row['date']?>">
+            </div>
+
+            <div class="inlinecontentC">
+                <h3>Change CV : </h3>
+                <input type="text" name="file" value="<?php echo $row['time']?>">                
+            </div>
+            
+            <input type="submit" value="Update" name="update">
+
+            
+            
+        </form>
+        <div class="btn">
+                <a href="interviewsE.php"><button>Go Back</button></a>                             
+        </div>
+        <?php
+            }
+        }?>
+    </div>
+
+
+    <?php
+        if(isset($_POST['update'])){
+            $fullName = mysqli_real_escape_string($conn, $_POST['fullName']);
+            $contact = mysqli_real_escape_string($conn, $_POST['contact']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $dob = mysqli_real_escape_string($conn, $_POST['dob']);
+
+            
+            $sql = "UPDATE job_applications SET full_name='$fullName',contact_number='$contact',email='$email',dob='$dob',resume_path='$file_path' WHERE application_id='$applicationId'";
+ 
+            if(mysqli_query($conn,$sql)){
+                header("Location: viewApplication.php?id=$applicationId");
+            }
+            else{
+                echo "Error";
+            }
+        }   
+    ?>
+
+
+    <?php include 'footer.php';?>
+</body>
+</html>
