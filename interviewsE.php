a66e5acbfd1b2d71d280fbf799ad2072a934d265
hasarangad <hasarangadias@gmail.com>
2024-01-05 19:43:50 +0530

    Update interviewsE.php

--- interviewsE.php
+++ interviewsE.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checking CV</title>
+    <title>Careerbridge | Interviews</title>
     <link rel="stylesheet" type="text/css" href="CSS/checkCVStyle.css">
 </head>
 <body>
